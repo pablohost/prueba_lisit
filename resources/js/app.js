e621c4d90ffc72f69159df9df3282b95033e5a4b
Pablo Henriquez <pablo.host@hotmail.com>
2023-07-12 06:56:16 -0400

    start login

--- resources/js/app.js
+++ resources/js/app.js
@@ -35,4 +35,5 @@ app.component('example-component', ExampleComponent);
  * an "id" attribute of "app". This element is included with the "auth"
  * scaffolding. Otherwise, you will need to add an element yourself.
  */
+
 app.mount('#app');
